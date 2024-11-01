c0940cbbb46e2e280430891dc5a71ec64f2aa5b3
irfanfauzi558 <72026124+irfanfauzi558@users.noreply.github.com>
2024-11-01 23:50:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Fungsi untuk mempersiapkan audio tetapi tidak memutarnya
-function prepareMusic() {
-    const music = document.getElementById('birthdayMusic');
-    music.load();
-}
-
-// Fungsi untuk memutar audio saat ada interaksi pengguna
-function playMusic() {
-    const music = document.getElementById('birthdayMusic');
-    if (music.paused) {
-        music.play();
-    }
-}
-
-// Fungsi untuk menampilkan pesan spesial
 function revealMessage() {
     const message = document.getElementById('message');
     message.style.display = 'block';
